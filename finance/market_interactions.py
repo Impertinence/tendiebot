6dbb34e7fcca65e96a89af7ec5e6f39852c05369
Impertinence <p4ndemonium@protonmail.com>
2020-12-20 15:41:27 -0500

    test

--- finance/market_interactions.py
+++ finance/market_interactions.py
@@ -2,32 +2,8 @@
 
 import requests
 import pymongo
+import robin_stocks
 
-from bs4 import BeautifulSoup
-
-class Main():
-    def get_analyses():
-        print("asd")
-
-    def get_stock(self, ticker, source):
-        if(source == "yahoo"):
-            url = "https://finance.yahoo.com/quote/" + ticker + "?p=" + ticker + "&.tsrc=fin-srch"
-            r = requests.get(url)
-
-            #Parse html
-            soup = BeautifulSoup(r.text, 'html.parser')
-
-            #Stock information
-            current_price = float(soup.find("span", {"data-reactid": "50"}).string)
-            open_price = float(soup.find("span", {"data-reactid", "98"}).string)
-            # year_range = float(soup.find("td", {"data-test", "FIFTY_TWO_WK_RANGE-value"}))
-            
-        return [
-            current_price,
-            open_price
-        ]
-
-    def get_stock_historical(self, ticker, source):
-        historical_data = []
-        if(source == "yahoo"):
-            url = "https://finance.yahoo.com/quote/" + ticker + "/history?p=" + ticker
+class RobinHoodInteractions():
+	def __init__(self, username, password, expiresIn, scope, by_sms, store_session, mfa_code):
+		self.account_info = robin_stocks.authentication.login(username, password, expiresIn, scope, by_sms, store_session, mfa_code)
--- main.py
+++ main.py
@@ -5,5 +5,3 @@ import pymongo
 start_time = time.time()
 market_interactions = market_interactions.Main()
 
-print(market_interactions.get_stock("tsla", "yahoo"))
-print("time: " + (time.time() - start_time) + " seconds")
