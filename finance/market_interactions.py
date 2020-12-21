8de8c81429cc932d2fce74a09982675459dee0c9
Impertinence <p4ndemonium@protonmail.com>
2020-12-20 20:16:24 -0500

    robinhood account loings workin

--- finance/market_interactions.py
+++ finance/market_interactions.py
@@ -4,6 +4,18 @@ import requests
 import pymongo
 import robin_stocks
 
+from bson.json_util import dumps
+
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+miscellaneous = mongo_client["miscellaneous"]
+robinhood_credentials = miscellaneous["rh_creds"]
+
 class RobinHoodInteractions():
-	def __init__(self, username, password, expiresIn, scope, by_sms, store_session, mfa_code):
-		self.account_info = robin_stocks.authentication.login(username, password, expiresIn, scope, by_sms, store_session, mfa_code)
+	def __init__(self, username, password):
+		rh_credentials = list(robinhood_credentials.find())
+		if(len(rh_credentials) == 0):
+			if(robinhood_credentials.insert_one(self.rh_account_info) != True):
+				print("error retrieving Robin Hood credentials...")
+
+		self.rh_account_info = robin_stocks.authentication.login(username=username, password=password, expiresIn=86400, scope="internal", by_sms=True, store_session=True, mfa_code=None)
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from finance import market_interactions
-import time
+#Written by Pranav Hegde
 import pymongo
 
-start_time = time.time()
-market_interactions = market_interactions.Main()
+from finance import market_interactions
 
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+#Interactions
+robinhood = market_interactions.RobinHoodInteractions("pranavhegde11@gmail.com", "ae0iuwRmna1851",)
+
+#DB Interactions
+stock_market = myclient["stock_market"]
+crypto_market = myclient["crypto_market"]
+miscellaneous = myclient["miscellaneous"]
+
+print(robinhood)
