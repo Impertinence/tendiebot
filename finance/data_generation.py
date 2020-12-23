643adf10378a051e50e3aa84f0903a682499a4bb
Pranav Hegde <p4ndemonium@protonmail.com>
2020-12-22 19:31:55 -0500

    data generation

--- finance/data_generation.py
+++ finance/data_generation.py
@@ -12,12 +12,6 @@ market_data = mongo_client['market_data']
 
 robinhood_interactions = financial_interactions.RobinHoodInteractions("pranavhegde11@gmail.com", "ae0iuwRmna1851")
 
-#Utility function to parse data
-def parse_historical_data(tickers, historical_data):
-    for row in historical_data:
-        print(row.get('symbol'))
-
-
 class GenerateData():
     def PopulateCollections(self, data_type, params):
         print("Retrieving " + data_type + " data...")
@@ -28,4 +22,8 @@ class GenerateData():
         print("Retrieved " + data_type + " data...")
         print("Writing to database...")
 
-        parsed_historical_data = parse_historical_data(params[0], historical_data)
+        for row in historical_data:
+            collection = market_data['asset-' + row.get('symbol')]
+            collection.insert_one(row)
+        
+        print("Finished writing to database!")
