81f2f338f1a6797acf08a3cf48cd1b58c3e87611
perceivxl <pranavhegde3@gmail.com>
2021-03-22 15:11:24 -0400

    mongodb ingestion engine

--- crypto_ingestion.py
+++ crypto_ingestion.py
@@ -12,55 +12,56 @@ from misc import plotting
 
 #APIs
 public_client = cbp.PublicClient()
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-#Time
-now = datetime.datetime.now() + datetime.timedelta(hours=4)
-intervals = datetime.timedelta(minutes=300)
-previous = now - intervals
+#Relevant Databases and Collections
+crypto_db = mongo_client['crypto']
 
-#Historical entries (Minute granularity)
-btc_entries = []
-eth_entries = []
+btc_live = crypto_db['btc_live']
+eth_live = crypto_db['eth_live']
+
+btc_historical = crypto_db['btc_historical']
+eth_historical = crypto_db['eth_historical']
 
 #Live Entries (0.5 second granularity)
 btc_live = []
 eth_live = []
 
-#Initial Ingestion of last 35 hours of data
-
-#Bitcoin
-for x in range(0, 7):
-    entries = public_client.get_product_historic_rates("BTC-USD", previous, now, 60)
-    btc_entries.extend(entries)
-    previous -= intervals
-    now -= intervals
-    time.sleep(1)
-
-#Ethereum
-for x in range(0, 7):
-    entries = public_client.get_product_historic_rates("ETH-USD", previous, now, 60)
-    eth_entries.extend(entries)
-    previous -= intervals
-    now -= intervals
-    time.sleep(1)
-
 #Live Ingestion
 while True:
+    #Retrieve half-second entries
     new_eth_entry = public_client.get_product_ticker("ETH-USD")
     new_btc_entry = public_client.get_product_ticker("BTC-USD")
 
+    #Append half-second entries
     btc_live.append(new_btc_entry)
     eth_live.append(new_eth_entry)
 
+    #Analyses object
     btc_analyses = analyses.Analyses(btc_live)
     eth_analyses = analyses.Analyses(eth_live)
-btc_analyses.stochastics("bid", "ask", "price", 14, 3)
 
+    #Stochs
+    btc_stoch = btc_analyses.stochastics("bid", "ask", "price", 14, 3)
+    eth_stoch = eth_analyses.stochastics("bid", "ask", "price", 14, 3)
+
+    #Half-second pause before restarting loop
     time.sleep(0.5)
+
+#Time for historical ingestion
+now = datetime.datetime.now() + datetime.timedelta(hours=4)
+intervals = datetime.timedelta(minutes=300)
+previous = now - intervals
 
 #Historical ingestion
 while True:
-    new_eth_entries = public_client.get_product_ticker("ETH-USD")
-    new_btc_entries = public_client.get_product_ticker("BTC-USD")
+    new_eth_entries = public_client.get_product_historic_rates("ETH-USD", previous, now, 60)
+    new_btc_entries = public_client.get_product_historic_rates("BTC-USD", previous, now, 60)
+
+    #Publish to mongo database
+
+
+    previous -= intervals
+    now -= intervals
 
     time.sleep(60)
